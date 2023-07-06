6f635a7a322135f2cb903e5c7abe71ca8c62fb5a
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-07-06 18:57:25 +0530

    launcher: Launcher V3 Update
    ### Changelog
    • New ui interface
    • Added several new mods
    • Fixed all bugs noticed in previous update
    • If you use in your rom, please don't forget to put a credit for our work

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
-    rm -rf /data/resource-cache/*
-    rm -rf /data/system/package_cache/*
-    rm -rf /cache/*
-    rm -rf /data/dalvik-cache/*
+rm -rf /data/resource-cache/*
+rm -rf /data/system/package_cache/*
+rm -rf /cache/*
+rm -rf /data/dalvik-cache/*
+pm uninstall-system-updates com.miui.home
